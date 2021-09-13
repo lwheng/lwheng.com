d0ade84229ae81f5c1089f9bfe737692e0eaee83
Heng Low Wee <alpha116@gmail.com>
2021-09-14 00:03:13 +0800

    Try to call API Gateway

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,10 @@ module Main exposing (..)
 import Browser
 import Browser.Navigation as BN
 import Html
+import Html.Attributes as Attr
+import Html.Events as E
+import Http
+import Json.Encode as Encode
 import Url exposing (Url)
 
 
@@ -11,16 +15,21 @@ main =
 
 
 type alias Model =
-    Int
+    { name : String
+    , greeting : String
+    }
 
 
 type Msg
     = DoNothing
+    | CallAPI
+    | CaptureName String
+    | GotGreeting (Result Http.Error String)
 
 
 initModel : Model
 initModel =
-    0
+    { name = "", greeting = "" }
 
 
 init : Int -> Url -> BN.Key -> ( Model, Cmd Msg )
@@ -33,13 +42,47 @@ view model =
     { title = "lwheng.com"
     , body =
         [ Html.text "Hello, world! This is an Elm application"
+        , Html.textarea [ Attr.placeholder "Enter your name", E.onInput CaptureName ] []
+        , Html.button [ E.onClick CallAPI ] [ Html.text "Call API" ]
+        , Html.label [] [ Html.text "Greeting = " ]
+        , Html.label [] [ Html.text model.greeting ]
         ]
     }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update _ model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        DoNothing ->
+            ( model, Cmd.none )
+
+        CallAPI ->
+            ( model, callSayHello model )
+
+        CaptureName s ->
+            ( { model | name = s }, Cmd.none )
+
+        GotGreeting result ->
+            case result of
+                Ok ok ->
+                    ( { model | greeting = ok }, Cmd.none )
+
+                Err e ->
+                    case e of
+                        Http.BadUrl s ->
+                            ( { model | greeting = "Error: BadUrl " ++ s }, Cmd.none )
+
+                        Http.Timeout ->
+                            ( { model | greeting = "Error: Timeout" }, Cmd.none )
+
+                        Http.NetworkError ->
+                            ( { model | greeting = "Error: NetworkError" }, Cmd.none )
+
+                        Http.BadStatus code ->
+                            ( { model | greeting = "Error: BadStatus " ++ String.fromInt code }, Cmd.none )
+
+                        Http.BadBody s ->
+                            ( { model | greeting = "Error: BadBody " ++ s }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -55,3 +98,12 @@ onUrlRequest _ =
 onUrlChange : Url -> Msg
 onUrlChange _ =
     DoNothing
+
+
+callSayHello : Model -> Cmd Msg
+callSayHello model =
+    Http.post
+        { url = "https://lwheng-com-gateway-dl7mzoit.de.gateway.dev/hello"
+        , body = Http.jsonBody <| Encode.object [ ( "message", Encode.string model.name ) ]
+        , expect = Http.expectString GotGreeting
+        }
