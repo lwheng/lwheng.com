71f748b77da28af521f92dc834ebd8dde435950a
Heng Low Wee <alpha116@gmail.com>
2021-09-17 00:27:41 +0800

    Call API/Function using API key passed in via flags

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,13 @@ module Main exposing (..)
 
 import Browser
 import Browser.Navigation as BN
+import Element as E
+import Element.Background as EB
+import Element.Input as EI
 import Html
+import Http
 import Url exposing (Url)
+import Url.Builder as UrlBuilder
 
 
 main =
@@ -11,35 +16,62 @@ main =
 
 
 type alias Model =
-    Int
+    { greeting : String
+    , host : String
+    , apiKey : String
+    }
 
 
 type Msg
     = DoNothing
+    | CallAPI
+    | HandleHello (Result Http.Error String)
 
 
 initModel : Model
 initModel =
-    0
+    { greeting = "<No greeting yet>"
+    , host = ""
+    , apiKey = ""
+    }
 
 
-init : Int -> Url -> BN.Key -> ( Model, Cmd Msg )
-init _ _ _ =
-    ( initModel, Cmd.none )
+init : { host : String, apiKey : String } -> Url -> BN.Key -> ( Model, Cmd Msg )
+init flags _ _ =
+    ( { initModel | host = flags.host, apiKey = flags.apiKey }, Cmd.none )
 
 
 view : Model -> Browser.Document Msg
 view model =
     { title = "lwheng.com"
     , body =
-        [ Html.text "Hello, world! This is an Elm application"
+        [ E.layout [] <|
+            E.column []
+                [ E.text "Hello, world!"
+                , E.text "This is a Elm application."
+                , EI.button [ EB.color <| E.rgb255 238 238 238 ] { onPress = Just CallAPI, label = E.text "Click" }
+                , E.text model.greeting
+                ]
         ]
     }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update _ model =
-    ( model, Cmd.none )
+update msg model =
+    case msg of
+        DoNothing ->
+            ( model, Cmd.none )
+
+        CallAPI ->
+            ( model, callAPI model )
+
+        HandleHello res ->
+            case res of
+                Ok v ->
+                    ( { model | greeting = v }, Cmd.none )
+
+                Err err ->
+                    ( { model | greeting = "Error happened!" }, Cmd.none )
 
 
 subscriptions : Model -> Sub Msg
@@ -55,3 +87,26 @@ onUrlRequest _ =
 onUrlChange : Url -> Msg
 onUrlChange _ =
     DoNothing
+
+
+callAPI : Model -> Cmd Msg
+callAPI model =
+    let
+        url =
+            { protocol = Url.Https
+            , host = model.host
+            , port_ = Nothing
+            , path = UrlBuilder.absolute [ "hello" ] [ UrlBuilder.string "key" model.apiKey ]
+            , query = Nothing
+            , fragment = Nothing
+            }
+    in
+    Http.request
+        { method = "GET"
+        , headers = []
+        , url = Url.toString <| url
+        , body = Http.emptyBody
+        , expect = Http.expectString HandleHello
+        , timeout = Nothing
+        , tracker = Nothing
+        }
